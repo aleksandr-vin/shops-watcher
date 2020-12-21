cf9cd71aa48d2fc1aa45d45d5f0cf0a2f2d312c1
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2020-12-21 14:26:03 +0100

    Update script

--- vrcover-shop-eu-scraper.py
+++ vrcover-shop-eu-scraper.py
@@ -7,35 +7,27 @@ import sys
 import os
 
 import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(name)s %(levelname)s - %(message)s', level=logging.INFO)
 
-url = u"https://eu.vrcover.com/products/facial-interface-foam-replacement-set-for-oculus\u2122-quest-2-winter-edition"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-#print(soup)
-for l in soup.find_all('script', id="ProductJson-product"):
-
-    #from telegram.ext import Updater
-    #updater = Updater(token=os.getenv('VRCoverAvailabilityBot_TOKEN'), use_context=True)
-    #dispatcher = updater.dispatcher
-
-    # def start(update, context):
-    #     print(update.effective_chat.id)
-    #     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-    # from telegram.ext import CommandHandler
-    # start_handler = CommandHandler('start', start)
-    # dispatcher.add_handler(start_handler)
-    # updater.start_polling()
-    # updater.idle()
-
+def telegramming(msg):
     from telegram import Bot
     bot = Bot(token=os.getenv('VRCoverAvailabilityBot_TOKEN'))
     chat_id = 115668939 # my channel
+    bot.send_message(chat_id=chat_id, text=msg)
 
+
+logging.debug("Fetching page")
+url = u"https://eu.vrcover.com/products/facial-interface-foam-replacement-set-for-oculus\u2122-quest-2-winter-edition"
+response = requests.get(url)
+logging.info(f"Page fetched {len(response.content)} bytes in {response.elapsed.total_seconds()} seconds")
+soup = BeautifulSoup(response.text, "html.parser")
+for l in soup.find_all('script', id="ProductJson-product"):
     product = json.loads(l.string)
     for v in product['variants']:
-        print(f"{v['sku']} {'available' if v['available'] else 'not-available'}: {v['name']}: {url}", file = sys.stdout if v['available'] else sys.stderr)
+        logging.info(f"{v['sku']} {'available' if v['available'] else 'not-available'}: {v['name']}: {url}")
         if v['available']:
-            bot.send_message(chat_id=chat_id, text=f"{v['sku']} {'available' if v['available'] else 'not-available'}: {v['name']}: {url}")
+            logging.info("Product is available, telegramming")
+            telegramming(f"{v['sku']} {'available' if v['available'] else 'not-available'}: {v['name']}: {url}")
+        else:
+            logging.info("Product is not available, not telegramming")
+            # telegramming("Just checking")
